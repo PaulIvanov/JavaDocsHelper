647fd95933431a48942eb56a284a201205cdc21a
pavel.ivanov <paulivanov123@gmail.com>
2016-12-08 21:52:38 -0800

    Removed debug statement

--- app/src/main/java/com/example/paulivanovs/myapplication/HomePage.java
+++ app/src/main/java/com/example/paulivanovs/myapplication/HomePage.java
@@ -84,8 +84,7 @@ public class HomePage extends AppCompatActivity
      */
     {
         text_hello_world = (TextView) (findViewById(R.id.text_hello_world));
-        int idx = ++m_Word_Index % (m_Text_List.size());
-        text_hello_world.setText(m_Text_List.get(idx));
+        text_hello_world.setText(m_Text_List.get(++m_Word_Index % (m_Text_List.size))); //mod by size to prevent out of index error
 
     }
 }
