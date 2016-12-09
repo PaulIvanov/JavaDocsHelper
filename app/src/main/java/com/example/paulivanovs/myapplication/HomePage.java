4ffe379733573ad89f2ad192130edabd55e9b937
Isak Lisoff <ilisoff@gmail.com>
2016-12-08 20:02:32 -0800

    Added Hello World to HomePage

--- app/src/main/java/com/example/paulivanovs/myapplication/HomePage.java
+++ app/src/main/java/com/example/paulivanovs/myapplication/HomePage.java
@@ -8,20 +8,28 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class HomePage extends AppCompatActivity {
+public class HomePage extends AppCompatActivity
+{
+    TextView text_hello_world;
+    //Button btn_hello_world;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
@@ -29,24 +37,35 @@ public class HomePage extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home_page, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void HelloAction(View v)
+    {
+        text_hello_world = (TextView) (findViewById(R.id.text_hello_world));
+        //btn_hello_world = (Button) (findViewById(R.id.btn_hello_world));
+        text_hello_world.setText("Hello World!");
+
     }
 }
